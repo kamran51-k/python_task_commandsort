fb475d0ed38f1d6cb56b10497c668f255e944ed0
kamran51-k <85585311+kamran51-k@users.noreply.github.com>
2021-08-14 12:34:49 +0400

    Update solved4.py

--- solved4.py
+++ solved4.py
@@ -1,11 +1,15 @@
-a = input()
-a = a.split(' ')
-list1 = []
-for i in a:
-    i = int(i)
-    list1.append(i) 
-    list1 = sorted(list1)
-print(list1)    
+def command_sort():
+    a = input()
+    a = a.split(' ')
+    list1 = []
+    for i in a:
+        i = int(i)
+        list1.append(i) 
+        list1 = sorted(list1)
+    print(list1)
+command_sort()
+
+    
 
     
     
